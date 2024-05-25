21ba2878a9c44584e3824d8f23195206cc8b0958
Prabin Khatri <prabin.khatri@gmail.com>
2024-05-25 12:48:49 +0545

    removed client id and client secret code

--- include/config.php
+++ include/config.php
@@ -7,12 +7,9 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'google_calendar'); 
  
 
-define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/calendar'); 
 define('REDIRECT_URI', 'http://localhost/googlecalendar/google_calendar_event_sync.php'); 
  
 // Start session 
 if(!session_id()) session_start(); 
- 
-$googleOauthURL = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode(GOOGLE_OAUTH_SCOPE) . '&redirect_uri=' . REDIRECT_URI . '&response_type=code&client_id=' . GOOGLE_CLIENT_ID . '&access_type=online'; 
- 
+
 ?>
